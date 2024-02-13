11a2cb8e590181651d0a961ebaddf488140efcb3
jakubfiglak <jakub.figlak@netguru.com>
2024-02-13 15:53:30 +0100

    feat: search setlist fm

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,21 @@
 import { SETLIST_FM_API_KEY } from '$env/static/private';
 
-export async function load() {
-	const url = new URL('https://api.setlist.fm/rest/1.0/setlist/5bb9abe0');
+export async function load({ url }) {
+	const search = url.searchParams.get('search');
 
-	const response = await fetch(url, {
+	if (!search) {
+		return { setlists: [] };
+	}
+
+	const setlistFmUrl = new URL('https://api.setlist.fm/rest/1.0/search/setlists');
+
+	setlistFmUrl.searchParams.append('artistName', search);
+
+	const response = await fetch(setlistFmUrl, {
 		headers: { 'x-api-key': SETLIST_FM_API_KEY, accept: 'application/json' }
 	});
 
 	const data = await response.json();
 
-	return { data };
+	return { setlists: data };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,55 @@
-<script>
+<script lang="ts">
+	import { page, navigating } from '$app/stores';
+	import { goto } from '$app/navigation';
+
 	export let data;
+
+	let search = $page.url.searchParams.get('search') ?? '';
+
+	async function handleSubmit() {
+		const params = new URLSearchParams();
+
+		params.append('search', search);
+
+		await goto(`?${params.toString()}`, { keepFocus: true });
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<pre>{JSON.stringify(data, null, 2)}</pre>
+<div class="container">
+	<h1>Welcome to <span>SpotiFM</span></h1>
+	<form on:submit|preventDefault={handleSubmit}>
+		<input name="search" placeholder="Search term..." bind:value={search} />
+		<input type="submit" value="Search" />
+	</form>
+
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+</div>
+
+<style>
+	.container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 5rem;
+		gap: 1rem;
+	}
+
+	h1 span {
+		color: var(--primary);
+	}
+
+	form {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	input {
+		border-radius: 0.5rem;
+		border: 1px solid var(--input);
+		padding: 0.25rem 0.75rem;
+		width: 16rem;
+	}
+</style>
