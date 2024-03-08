e6f65eb59f5b783ad74b5f70f8930f97029c1c95
Jakub Figlak <jakub.figlak@gmail.com>
2024-03-08 12:09:15 +0100

    tests/

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,5 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+console.log("test")
